10a3f659104690de1c3aa587f15be430977709bc
dren-haziri <108676090+dren-haziri@users.noreply.github.com>
2022-12-06 09:10:20 +0100

    Add project files.

---
+++ Jobsway2go.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jobsway2go.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

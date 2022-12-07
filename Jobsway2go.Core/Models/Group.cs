01c0cf47457984a1e12913d72cbffa5e9878d53e
Arianit_Ajvazi <arianitajvazi@gmail.com>
2022-12-07 13:43:01 +0100

    Class Added for Goup

---
+++ Jobsway2go.Core/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Core.Models
+{
+    public class Group
+    {
+        public int UUId { get; set; }
+        public string Name { get; set; }
+        //public List<AdminUser> Admin {get;set;}
+        //public List<ModeratoUser> Moderator {get;set;}
+    }
+}

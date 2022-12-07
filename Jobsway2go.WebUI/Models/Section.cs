eff7a0eff13416dec558a2b749f56c7039fb6be9
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-07 13:41:35 +0100

    Created section model

--- Jobsway2go.WebUI/Models/Section.cs
+++ Jobsway2go.WebUI/Models/Section.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
-public class Class1
+[DataContract]
+public class Section
 {
-	public Class1()
-	{
-	}
+    [DataMember]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    [String]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Location is required")]
+    [String]
+    public string Location { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:d}")]
+    public DateTime DateFrom { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:d}")]
+    public DateTime DateTo { get; set; }
+
 }

a10adda94dd58c4ead00c9149df1d6faab5f22fe
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-07 14:59:13 +0100

    Updated Section

--- Jobsway2go.WebUI/Models/Section.cs
+++ Jobsway2go.WebUI/Models/Section.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 [DataContract]
 public class Section
 {
     [DataMember]
     public int Id { get; set; }
-
     public string Name { get; set; }
-
     public string Location { get; set; }
-
     public DateTime DateFrom { get; set; }
-
     public DateTime DateTo { get; set; }
 
 }

f41959364a4a1ec509b8835515664a5eab5ce6a1
MedinaRexhepi <102358051+MedinaRexhepi@users.noreply.github.com>
2022-12-07 16:38:32 +0100

    Added Notification model and contract implementation

---
+++ Jobsway2go.Core/Models/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Core.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null;
+        public DateTime Date { get; set; }
+
+    }
+}

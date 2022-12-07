5562466c0501578a84c987cfe213081199ef9e84
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-07 15:10:54 +0100

    Updated section

--- Jobsway2go.WebUI/Models/Section.cs
+++ Jobsway2go.WebUI/Models/Section.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-[DataContract]
 public class Section
 {
-    [DataMember]
     public int Id { get; set; }
     public string Name { get; set; }
     public string Location { get; set; }
     public DateTime DateFrom { get; set; }
     public DateTime DateTo { get; set; }
-
 }

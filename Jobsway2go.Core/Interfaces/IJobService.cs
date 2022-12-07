2fb5489db377fe303a812778873fa8014411d34e
Diell02 <diellkepuska@gmail.com>
2022-12-07 12:51:05 +0100

    Job, JobCategory Model + IJobService

---
+++ Jobsway2go.Core/Interfaces/IJobService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Core.Interfaces
+{
+    internal interface IJobService
+    {
+        public void SearchJob();
+        public void FilterByCategory(string category);
+        public void FilterByCity(string city);
+        public void ApplyForJob();
+        public void UserSaveJob();
+    }
+}

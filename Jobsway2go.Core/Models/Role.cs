96a6a9129a9d316f460bc24b5def7a27b3d32058
BardhShabanii <bardhi0105@gmail.com>
2022-12-07 14:27:55 +0100

    Role model and contract implementation

---
+++ Jobsway2go.Core/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Core.Models
+{
+    public class Role:IdentityRole
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}

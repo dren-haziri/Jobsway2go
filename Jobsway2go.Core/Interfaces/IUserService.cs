7f6a41dbc976cf43526aa750eec2c293f88a43d0
Muhamet1 <mh51221@ubt-uni.net>
2022-12-07 13:33:56 +0100

    Added Application User Model, Interface

---
+++ Jobsway2go.Core/Interfaces/IUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Core.Interfaces
+{
+    public interface IUserService
+    {
+        public Boolean SignIn { get; set; }
+        public Boolean SignOut { get; set; }
+
+        public Boolean Register { get; set; }
+        public Boolean EmailVerification { get;set; }
+        public Boolean EmailConfirmed { get; set; }
+
+        public Boolean EditProfile { get; set; }
+        public Boolean VerifyUser { get; set; } 
+    }
+}

6ce27a0bfd57a2ddba099a3d22be83954285aef2
Agnesa <agnesa.28@hotmail.com>
2022-12-08 03:11:45 +0100

    Added post model and contract

---
+++ Jobsway2go.Core/Interfaces/IPost.cs
@@ -0,0 +1,9 @@
+﻿using Jobsway2go.Core.Models;
+
+namespace Jobsway2go.Core.Interfaces;
+
+public interface IPost : IRepository<Post>
+{
+    public void SearchPost();
+    public void FilterByType(PostType type);
+}
---
+++ Jobsway2go.Core/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace Jobsway2go.Core.Models;
+
+public class Post
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedAtUTC { get; set; }
+    public string CreatedByUserId { get; set; }
+    public PostType Type { get; set; }
+}
+
+public enum PostType
+{
+    Standard, 
+    Premium
+}

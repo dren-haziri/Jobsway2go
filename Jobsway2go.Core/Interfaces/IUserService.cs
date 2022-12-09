7e778deb1277df14531ee6a47d4b64c2aad26c73
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-09 15:32:42 +0100

    Created Activate and Deactivate methods in IUserSection

--- Jobsway2go.Core/Interfaces/IUserService.cs
+++ Jobsway2go.Core/Interfaces/IUserService.cs
@@ -8,16 +8,19 @@ namespace Jobsway2go.Core.Interfaces
 {
     public interface IUserService
     {
-         bool SignIn { get; set; }
-         bool SignOut { get; set; }
+        bool SignIn { get; set; }
+        bool SignOut { get; set; }
 
-        bool   Register { get; set; }
-        bool  EmailVerification { get;set; }
-        bool  EmailConfirmed { get; set; }
+        bool Register { get; set; }
+        bool EmailVerification { get; set; }
+        bool EmailConfirmed { get; set; }
 
-        bool  EditProfile { get; set; }
-        bool  VerifyUser { get; set; } 
+        bool EditProfile { get; set; }
+        bool VerifyUser { get; set; }
 
-        Task<bool> DeleteUser (string userId);
-    }
+        Task<bool> DeleteUser(string userId);
+
+        Task<bool> Activate(string userId);
+        Task<bool> Deactivate(string userId);
+    }       
 }

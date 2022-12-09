1a9c8354df77430c3afcfad79e177e8fac74bdf0
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-09 15:34:23 +0100

    Defined Activate and Deactivate methods in UserController

--- Jobsway2go.WebUI/Controllers/UserController.cs
+++ Jobsway2go.WebUI/Controllers/UserController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Jobsway2go.Core.Interfaces;
+using Jobsway2go.Core.Models;
 
 namespace Jobsway2go.WebUI.Controllers
 {
@@ -7,6 +10,31 @@ namespace Jobsway2go.WebUI.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<bool> Activate(string userId)
+        {
+            ApplicationUser user = _userManager.FindByIdAsync(userId);
+            user.await();
+            var result = user.Result;
+            if (result)
+            {
+                user.isActive = true;
+                return true;
+            }
+            return false;
+        }
+        public async Task<bool> Deactivate(string userId)
+        {
+            ApplicationUser user = _userManager.FindByIdAsync(userId);
+            user.await();
+            var result = user.Result;
+            if (result)
+            {
+                user.isActive = false;
+                return true;
+            }
+            return false;
         }
     }
 }

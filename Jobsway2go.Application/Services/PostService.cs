0c08be2bb59f1cf57d97433a73d315ba1fdc820f
Diell02 <diellkepuska@gmail.com>
2022-12-12 14:03:58 +0100

    PostService, PostController
    PostService- Implemented the methods & generated constructor
    PostController- Created the methods and commented them until some Async methods are added
    IPost name change IPostService

---
+++ Jobsway2go.Application/Services/PostService.cs
@@ -0,0 +1,88 @@
+﻿using Jobsway2go.Core.Enums;
+using Jobsway2go.Core.Interfaces;
+using Jobsway2go.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Application.Services
+{
+    public class PostService : IPostService
+    {
+        public readonly Post _post;
+
+        public PostService(Post post)
+        {
+            _post = post;
+        }
+
+        public void Add(Post entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<Post> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FilterByType(PostType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> Find(Expression<Func<Post, bool>> predicate, 
+                                        params Expression<Func<Post, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post FindOne(Expression<Func<Post, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Post entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Post> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SearchPost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRange(IEnumerable<Post> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Jobsway2go.WebUI/Controllers/PostController.cs
@@ -0,0 +1,125 @@
+﻿using Jobsway2go.Core.Enums;
+using Jobsway2go.Core.Interfaces;
+using Jobsway2go.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jobsway2go.WebUI.Controllers
+{ 
+    public class PostController : Controller
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var posts = _postService.GetAll();
+
+            return View(posts);
+        }
+        /*
+        [HttpPost]
+        public async Task<IActionResult> Create(Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _postService.Add(post);
+            await _postService.SaveChangesAsync();
+
+            return Ok(post);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Read(int id)
+        {
+            // Get the post by id
+            var post = await _postService.GetByIdAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+        
+        [HttpPut]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            // Validate the post model
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Get the existing post
+            var existingPost = await _postService.GetByIdAsync(post.Id);
+            if (existingPost == null)
+            {
+                return NotFound();
+            }
+
+            // Update the post
+            existingPost.Title = post.Title;
+            existingPost.Description = post.Description;
+            existingPost.CreatedAtUTC = post.CreatedAtUTC;
+            existingPost.CreatedByUserId = post.CreatedByUserId;
+            existingPost.Type = post.Type;
+
+            // Save the changes to the database
+            await _postService.SaveChangesAsync();
+
+            return Ok(existingPost);
+            }
+        }
+        
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            // Get the post by id
+            var post = await _postService.GetByIdAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            // Delete the post
+            _postService.Remove(post);
+
+            // Save the changes to the database
+            await _postService.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchPost(string query)
+        {
+            // Search for posts that match the given query
+            var posts = await _postService.Find(
+                p => p.Title.Contains(query) || p.Description.Contains(query),
+                p => p.CreatedByUserId
+            );
+
+            return Ok(posts);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FilterByType(PostType type)
+        {
+            // Filter posts by type
+            var posts = await _postService.Find(
+                p => p.Type == type,
+                p => p.CreatedByUserId
+            );
+
+            return Ok(posts);
+        }*/
+    }
+}

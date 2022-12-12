20de97afcfa57f7413ac02a5acd32bed961061d7
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-12 16:33:06 +0100

    Created ICollectionService

---
+++ Jobsway2go.Core/Interfaces/ICollectionService.cs
@@ -0,0 +1,14 @@
+﻿using Jobsway2go.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Core.Interfaces
+{
+    public interface ICollectionService : IRepository<Collection>
+    {
+        public void SaveToCollection();
+    }
+}

1303a853d4062e4066d5a74c475fb6c75f253299
Diell02 <diellkepuska@gmail.com>
2022-12-13 10:03:09 +0100

    PostService, Controller, View

---
+++ Jobsway2go.Application/Services/PostService.cs
@@ -0,0 +1,107 @@
+﻿using Jobsway2go.Core.Enums;
+using Jobsway2go.Core.Interfaces;
+using Jobsway2go.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Application.Services
+{
+    public class PostService : IPostService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public PostService(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void Add(Post entity)
+        {
+            _context.Posts.Add(entity);
+            _context.SaveChangesAsync();
+        }
+
+        public void AddRange(IEnumerable<Post> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FilterByType(PostType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> Find(Expression<Func<Post, bool>> predicate, params Expression<Func<Post, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post FindOne(Expression<Func<Post, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            return _context.Posts.ToList();
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Posts.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(Post entity)
+        {
+            var post = _context.Posts.FirstOrDefault(g => g.Id == entity.Id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+                _context.SaveChangesAsync();
+            }
+        }
+
+        public void RemoveRange(IEnumerable<Post> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SearchPost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post entity)
+        {
+            var post = _context.Posts.FirstOrDefault(x => x.Id == entity.Id);
+            if (post != null)
+            {
+                post.Title = entity.Title;
+                post.Description = entity.Description;
+                post.CreatedAtUTC = entity.CreatedAtUTC;
+                post.CreatedByUserId = entity.CreatedByUserId;
+                post.Type = entity.Type;
+                _context.Posts.Update(post);
+                _context.SaveChangesAsync();
+            }
+        }
+
+        public void UpdateRange(IEnumerable<Post> entity)
+        {
+            throw new NotImplementedException();
+        }
+        public void Create(Post post)
+        {
+            Add(post);
+            _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Jobsway2go.WebUI/Controllers/PostController.cs
@@ -0,0 +1,96 @@
+﻿using Jobsway2go.Application.Services;
+using Jobsway2go.Core.Interfaces;
+using Jobsway2go.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jobsway2go.WebUI.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index()
+        {
+            var posts = _postService.GetAll();
+            return View(posts);
+        }
+        public IActionResult Details(int id)
+        {
+            var post = _postService.GetById(id);
+            return View(post);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _postService.Add(post);
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        public IActionResult Update(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var post = _postService.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _postService.Update(post);
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var post = _postService.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Post post)
+        {
+
+            _postService.Remove(post);
+            return RedirectToAction("Index");
+        }
+    }
+}

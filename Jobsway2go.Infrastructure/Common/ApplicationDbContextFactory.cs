2785d634982e9f988076cd2e39de931804f901fc
Rron <rrqirezi@gmail.com>
2022-12-13 11:12:41 +0100

    Added Scaffolded items in views and controllers. Removed the service with identity from program.cs

---
+++ Jobsway2go.Infrastructure/Common/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2go.Infrastructure.Common
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=sql.bsite.net\\MSSQL2016;Initial Catalog=drenh_jobportal;User ID=drenh_jobportal;Password=jobPortal;Persist Security Info=True;TrustServerCertificate=true");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
